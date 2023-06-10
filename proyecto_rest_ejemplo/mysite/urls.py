7f1f834bd267435cfcfa1138be3a011bd49d3779
yurley <yurs.ksf1@gmail.com>
2023-06-10 08:52:22 -0500

    ejemplo filtros

--- proyecto_rest_ejemplo/mysite/urls.py
+++ proyecto_rest_ejemplo/mysite/urls.py
@@ -34,5 +34,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
-
 ]
